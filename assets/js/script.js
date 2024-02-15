bd7d0eb18af1dd7d3d25c7031b4c4b34a39bdba9
William Dias <smdwilliam89@gmail.com>
2024-02-15 18:40:23 -0300

    feat(script): Display addition question to the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,30 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('You clicked Submit!');
       } else {
         let gameType = this.getAttribute('data-type');
-        console.log(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+  runGame('addition');
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-const runGame = () => {
+const runGame = (gameType) => {
   // Creates two random numbers between 1 and 25;
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
-};
 
-runGame();
+  if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+  }
+};
 
 const checkAnswer = () => {};
 
@@ -36,7 +43,11 @@ const incrementScore = () => {};
 
 const incrementWrongAnswer = () => {};
 
-const displayAdditionQuestion = () => {};
+const displayAdditionQuestion = (operand1, operand2) => {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
+};
 
 const displaySubtractionQuestion = () => {};
 
