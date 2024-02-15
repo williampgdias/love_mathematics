71dbbbdd5a478f6627f80a349e50ca183f4fab1b
William Dias <smdwilliam89@gmail.com>
2024-02-15 16:24:55 -0300

    feat(script): Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const runGame = () => {};
+
+const checkAnswer = () => {};
+
+const calculateCorrectAnswer = () => {};
+
+const incrementScore = () => {};
+
+const incrementWrongAnswer = () => {};
+
+const displayAdditionQuestion = () => {};
+
+const displaySubtractionQuestion = () => {};
+
+const displayMultiplyQuestion = () => {};
